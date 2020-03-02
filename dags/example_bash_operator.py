6297d6cb17136de2cbc75468fdfa7be5c137ae9a
ivan <ivan.sinitsyn@awok.com>
2020-03-02 14:15:38 +0400

    example_bash_operator schedule updated

--- dags/example_bash_operator.py
+++ dags/example_bash_operator.py
@@ -16,13 +16,15 @@ default_args = {
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG("example_bash_operator.py", default_args=default_args, schedule_interval=None)
+dag = DAG(
+    "example_bash_operator.py",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+    max_active_runs=1,
+)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
